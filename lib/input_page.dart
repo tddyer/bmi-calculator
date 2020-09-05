2cc6d6c8a08e35ee4949e64f9261548361909354
Tanner <tdyer@hawk.iit.edu>
2020-09-05 15:21:10 -0500

    built base ui using custom container widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,54 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  
+  ReusableCard({this.colour});
+
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour, // when using box decoration, color goes in here
+        borderRadius: BorderRadius.circular(10.0)
       ),
     );
   }
