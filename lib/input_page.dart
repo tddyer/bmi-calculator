e9f0f99e493bcb710e772fda25cd65eabf4866c8
Tanner <tdyer@hawk.iit.edu>
2020-09-12 22:01:39 -0500

    moved round icon button to separate file

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // icons
 import 'gender_card_content.dart';
+import 'round_icon_button.dart';
 import 'calculate_button.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
@@ -217,26 +218,3 @@ class _InputPageState extends State<InputPage> {
 }
 
 
-// custom button widget
-class RoundIconButton extends StatelessWidget {
-
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 0.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
