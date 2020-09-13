0f0d462668cd699e764619884acc7475cfa1e420
Tanner <tdyer@hawk.iit.edu>
2020-09-12 21:53:51 -0500

    added base layout with filler content on results page

--- lib/constants.dart
+++ lib/constants.dart
@@ -19,3 +19,23 @@ const kLargeButtonTextStyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultsTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultsBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -7,7 +9,45 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Text('Hello'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kReusableCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultsTextStyle,
+                  ),
+                  Text(
+                    '17.7',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your BMI result is perfectly healthy, keep up your regular habits!',
+                    textAlign: TextAlign.center,
+                    style: kResultsBodyTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
